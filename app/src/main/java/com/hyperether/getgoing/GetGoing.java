ab9e9d1feaf3200d2ddad7af1105ea333a336d1f
Slobodan Prijic <slobodan.prijic@gmail.com>
2014-11-17 22:31:37 +0100

    Dodata nova klasa, za prikaz nove stranice

--- app/src/main/java/com/hyperether/getgoing/GetGoing.java
+++ app/src/main/java/com/hyperether/getgoing/GetGoing.java
@@ -12,8 +12,18 @@ public class GetGoing extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        /*
+        Zadatak:
+        1. pronadji image buttone po id-u iz xml-a
+        2. dodeli svakom od njih onClickListener
+        3. u metodi onClick pozovi novu stranicu ShowLocation.class preko Intent-a i startActivity(...) metode
+        4. Ta nova stranica ShowLocation.class je napravljena da ucitava show_location.xml
+        5. Kad kliknes na dugme prebacice te na novi layout
+
+         */
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/com/hyperether/getgoing/location/ShowLocation.java
@@ -0,0 +1,17 @@
+package com.hyperether.getgoing.location;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.hyperether.getgoing.R;
+
+public class ShowLocation extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        // getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        // Keep screen on all the time
+        setContentView(R.layout.show_location);
+    }
+}
