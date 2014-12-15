79f18ca75b8d24c572197700a69d483160038f6e
Dreamgirl <nikolic.sanja86@gmail.com>
2014-12-15 14:32:16 +0100

    list_element port

--- app/src/main/java/com/hyperether/getgoing/GetGoing.java
+++ app/src/main/java/com/hyperether/getgoing/GetGoing.java
@@ -53,12 +53,7 @@ public class GetGoing extends Activity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.get_going, menu);
-        return true;
-    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
