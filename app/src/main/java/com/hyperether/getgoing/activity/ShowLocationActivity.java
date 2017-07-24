03a06b25a0532379fdfba33e1e1b9d1006de133c
nikola <qsnnikola@gmail.com>
2017-07-24 13:13:38 +0200

    - Ask user for high accuracy location mode.

--- app/src/main/java/com/hyperether/getgoing/activity/ShowLocationActivity.java
+++ app/src/main/java/com/hyperether/getgoing/activity/ShowLocationActivity.java
@@ -557,7 +557,7 @@ public class ShowLocationActivity extends Activity implements
             gpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
             networkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
 
-            if(!gpsEnabled || networkEnabled) {
+            if(!gpsEnabled || !networkEnabled) {
                 AlertDialog.Builder dialog = new AlertDialog.Builder(this);
                 dialog.setCancelable(false);
                 dialog.setMessage(getString(R.string.alert_dialog_message));
