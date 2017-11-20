268decfbfb1d78bf0d2184a7efe569d17e80b36b
NikolaHadzicNs <nikola.hadzic@hotmail.com>
2017-11-20 01:34:07 +0100

    GetGoingActivityTest and ShowDataActivityTest(need to check if clicked button delete list row) work for now

--- app/src/androidTest/java/com/hyperether/getgoing/GetGoingActivityTest.java
+++ app/src/androidTest/java/com/hyperether/getgoing/GetGoingActivityTest.java
@@ -1,37 +1,59 @@
 package com.hyperether.getgoing;
 
-import android.test.ActivityInstrumentationTestCase2;
-import android.test.suitebuilder.annotation.SmallTest;
-import android.widget.ImageButton;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
 import com.hyperether.getgoing.activity.GetGoingActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Created by nikola on 19.10.17..
  */
 
-public class GetGoingActivityTest extends ActivityInstrumentationTestCase2<GetGoingActivity> {
-    public GetGoingActivityTest() {
-        super(GetGoingActivity.class);
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class GetGoingActivityTest {
+
+    @Rule
+    public ActivityTestRule<GetGoingActivity> mActivityRule =
+            new ActivityTestRule(GetGoingActivity.class);
+
+    @Test
+    public void checkButtons(){
+        onView(withId(R.id.walk_button)).check(matches(isDisplayed()));
+        onView(withId(R.id.run_button)).check(matches(isDisplayed()));
+        onView(withId(R.id.ride_button)).check(matches(isDisplayed()));
     }
 
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
+    @Test
+    public void clickButtons(){
+        onView(withId(R.id.walk_button)).perform(click());
+        pressBack();
+        onView(withId(R.id.run_button)).perform(click());
+        pressBack();
+        onView(withId(R.id.ride_button)).perform(click());
     }
 
-    @SmallTest
-    public void testaddButtonListener(){
-        ImageButton imageButtonwalk = (ImageButton)getActivity().findViewById(R.id.walk_button);
-        ImageButton imageButtonrun = (ImageButton)getActivity().findViewById(R.id.walk_button);
-        ImageButton imageButtonride = (ImageButton)getActivity().findViewById(R.id.walk_button);
-        assertNotNull(imageButtonwalk);
-        assertNotNull(imageButtonride);
-        assertNotNull(imageButtonrun);
-    }
-
-    @Override
-    protected void tearDown() throws Exception {
-        super.tearDown();
+    @Test
+    public void clickOptions(){
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        onView(withText("Statistics")).perform(click());
+        pressBack();
+        onView(withId(R.id.action_settings)).perform(click());
+        pressBack();
     }
 }
--- app/src/androidTest/java/com/hyperether/getgoing/ShowLocationActivityTest.java
+++ app/src/androidTest/java/com/hyperether/getgoing/ShowLocationActivityTest.java
@@ -1,35 +1,34 @@
 package com.hyperether.getgoing;
 
-import android.test.ActivityInstrumentationTestCase2;
-import android.test.suitebuilder.annotation.SmallTest;
-import android.widget.Button;
-import android.widget.Chronometer;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
 import com.hyperether.getgoing.activity.ShowLocationActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * Created by nikola on 19.10.17..
  */
 
-public class ShowLocationActivityTest extends ActivityInstrumentationTestCase2<ShowLocationActivity> {
-    public ShowLocationActivityTest() {
-        super(ShowLocationActivity.class);
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class ShowLocationActivityTest {
+
+    @Rule
+    public ActivityTestRule<ShowLocationActivity> mActivityRule =
+            new ActivityTestRule(ShowLocationActivity.class);
+
+    @Test
+    public void checkButtons() {
+
     }
 
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-    }
+    @Test
+    public void clickButtons() {
 
-    @SmallTest
-    public void testComponents(){
-        Button button_start;
-        button_start = (Button)getActivity().findViewById(R.id.start_button);
-        assertNotNull(button_start);
-    }
-
-    @Override
-    protected void tearDown() throws Exception {
-        super.tearDown();
     }
 }
